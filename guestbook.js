6b1322f03d9ffd7493517db45eb396c55ac72a36
zvdas <67303760+zvdas@users.noreply.github.com>
2022-12-31 13:58:47 +0530

    / removed after guestbook for proper path

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/5dd18cd5-78c1-4a53-a210-80a90eeab19c/guestbook/';
+const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/5dd18cd5-78c1-4a53-a210-80a90eeab19c/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
